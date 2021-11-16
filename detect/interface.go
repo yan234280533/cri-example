36e46666d0738201500ae2db9a6a9f484ec1d839
devinyan <devinyan@tencent.com>
2021-11-16 14:56:15 +0800

    feat: add node and pod resource action

--- detect/interface.go
+++ detect/interface.go
@@ -12,6 +12,8 @@ type DetectionCondition struct {
 	Namespace string
 	// if the policy triggered action
 	Triggered bool
+	// if the policy triggered restored action
+	Restored bool
 	// the influenced pod list, which is used to determine which pods should to do action
 	// node detection the pod list is empty
 	BeInfluencedPods []types.NamespacedName
@@ -23,36 +25,50 @@ type DetectionInterface interface {
 }
 
 type AvoidanceInterface interface {
-	AvoidanceActionMerge(detectConditions []DetectionCondition) (*AvoidanceActionStruct, error)
+	AvoidanceActionMerge(podDetectConditions []DetectionCondition, nodeDetectConditions []DetectionCondition) (*AvoidanceActionStruct, error)
 }
 
 type AvoidanceActionStruct struct {
 	BlockScheduledAction *BlockScheduledActionStruct
-	RetainActions        []RetainActionStruct
+	ThrottleActions      []CPUThrottleActionStruct
 	EvictActions         []EvictActionStruct
 }
 
 type BlockScheduledActionStruct struct {
+	BlockScheduledQOSPriority   *ScheduledQOSPriority
+	RestoreScheduledQOSPriority *ScheduledQOSPriority
+}
+
+type ScheduledQOSPriority struct {
 	PodQOSClass        v1.PodQOSClass
 	PriorityClassValue uint64
 }
 
-type CPURestrainActionStruct struct {
+type CPUThrottleActionStruct struct {
+	CPUDownAction *CPURatioStruct
+	CPUUpAction   *CPURatioStruct
+}
+
+type CPURatioStruct struct {
+	//the min of cpu ratio for pods
+	// +optional
+	MinCPURatio uint64 `json:"minCPURatio,omitempty"`
+
 	//the step of cpu share and limit for once down-size (1-100)
 	// +optional
 	StepCPURatio uint64 `json:"stepCPURatio,omitempty"`
 }
 
-type MemoryRestrainActionStruct struct {
+type MemoryThrottleActionStruct struct {
 	// to force gc the page cache of low level pods
 	// +optional
 	ForceGC bool `json:"forceGC,omitempty"`
 }
 
-type RetainActionStruct struct {
-	CPURestrain    *CPURestrainActionStruct
-	MemoryRestrain *MemoryRestrainActionStruct
-	RetainPods     []types.NamespacedName
+type ThrottleActionStruct struct {
+	CPUThrottle    *CPUThrottleActionStruct
+	MemoryThrottle *MemoryThrottleActionStruct
+	ThrottlePods   []types.NamespacedName
 }
 
 type EvictActionStruct struct {
@@ -60,8 +76,10 @@ type EvictActionStruct struct {
 	EvictPods                  []types.NamespacedName
 }
 
-func AvoidanceActionMerge(detectConditions []DetectionCondition) (*AvoidanceActionStruct, error) {
+func AvoidanceActionMerge(podDetectConditions []DetectionCondition, nodeDetectConditions []DetectionCondition) (*AvoidanceActionStruct, error) {
 	//step1 do BlockScheduled merge
-	//step2 do Retain merge
+	//step2 do Retain merge FilterAndSortRestrainPods
 	//step3 do Evict merge  FilterAndSortEvictPods
+
+	return &AvoidanceActionStruct{}, nil
 }
